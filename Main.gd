65c68f6f577fa911ecae0fe1a6b1513a00f0476c
Luke Merrett <luke.merrett@just-eat.com>
2019-07-06 13:37:20 +0100

    Finished tutorial code

--- Main.gd
+++ Main.gd
@@ -16,6 +16,7 @@ func new_game():
 	$Player.start($StartPosition.position)
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
+	$Music.play()
 	$StartTimer.start()
 
 # Trigged by the "hit" signal on the player
@@ -23,6 +24,8 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 # When the start timer finishes (one shot)
 func _on_StartTimer_timeout():
