9e4f9b141eb8e0dfe48a6f6479bc9db139b5c309
Luke Merrett <luke.merrett@just-eat.com>
2019-07-06 11:46:26 +0100

    Hooked up timers and mobs

--- Main.gd
+++ Main.gd
@@ -19,3 +19,33 @@ func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+
+# When the start timer finishes (one shot)
+func _on_StartTimer_timeout():
+	# Start the other 2 timers
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+# When the score timer loops
+func _on_ScoreTimer_timeout():
+	# You score by surviving, 1 point per second
+	score += 1
+
+# When the mob timer loops
+func _on_MobTimer_timeout():
+	# Choose a random location on Path2D, which is at the borders of the game area
+	$MobPath/MobSpawnLocation.set_offset(randi())
+	# Create a Mob instance and add it to the scene.
+	var mob = Mob.instance()
+	add_child(mob)
+	# Set the mob's direction perpendicular to the path direction.
+	var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
+	# Set the mob's position to a random location.
+	mob.position = $MobPath/MobSpawnLocation.position
+	# Add some randomness to the direction.
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	# Set the velocity (speed & direction).
+	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = mob.linear_velocity.rotated(direction)
+
