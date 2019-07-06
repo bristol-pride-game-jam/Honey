dc7d2fd669f22a0cbd74009ef495a8659cae3969
Luke Merrett <luke.merrett@just-eat.com>
2019-07-06 16:30:54 +0100

    Limited play area

--- Player.gd
+++ Player.gd
@@ -6,6 +6,11 @@ signal hit
 
 # "export" makes this valuable available in the inspector, so we can play about with it quickly
 export var speed = 400  # How fast the player will move (pixels/sec).
+
+# To stop sprite going over edges
+export var sprite_size = Vector2(94, 45)
+export var min_height = 450
+
 var screen_size  # Size of the game window.
 
 # Called when the node enters the scene tree
@@ -37,8 +42,8 @@ func _process(delta):
 	# clamp stops the player going out of the bounds of the screen
 	# syntax is clamp(value, min, max)
 	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
+	position.x = clamp(position.x, (0 + sprite_size.x), (screen_size.x - sprite_size.x))
+	position.y = clamp(position.y, (0 + min_height + sprite_size.y), (screen_size.y - sprite_size.y))
 
 	# Flips the sprite based on which direction it is going
 	if velocity.x != 0:
