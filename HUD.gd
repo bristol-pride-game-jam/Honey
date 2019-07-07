661780752bae37d7c1ba059fecf640ea691ca7b2
Luke Merrett <luke.merrett@just-eat.com>
2019-07-07 10:54:23 +0100

    Added time label

--- HUD.gd
+++ HUD.gd
@@ -14,9 +14,17 @@ func _on_StartButton_pressed():
 	$StartButton.hide()
 	$MessageLabel.hide()
 	$Logo.hide()
+	
 	$ScoreLabel.text = "Score: 0"
 	$ScoreLabel.show()
+	
+	$TimeLabel.text = "Seconds Left: 0"
+	$TimeLabel.show()
+	
 	emit_signal("start_game")
+
+func change_seconds(seconds_left):
+	$TimeLabel.text = "Seconds Left: " + seconds_left
 
 func change_score(score):
 	$ScoreLabel.text = "Score: " + str(score)
