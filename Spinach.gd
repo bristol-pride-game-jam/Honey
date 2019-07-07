544cf48d6e2f2227e0c7428ed7de98f6e14f936d
Luke Merrett <luke.merrett@just-eat.com>
2019-07-07 09:32:56 +0100

    Stopped other leaves causing the score to jump

--- Spinach.gd
+++ Spinach.gd
@@ -6,7 +6,8 @@ func _ready():
 	pass
 
 func _on_Spinach_area_entered(area):
-	emit_signal("eaten") # Tell everyone it's been eaten
-	hide() # Hide the spinach
-	$CollisionShape2D.disabled = true # Stop any more collisions
-	queue_free() # Remove spinach now it's been eaten!
+	# Stop other leaves causing the score to jump
+	if area.get_name() == "Player":
+		emit_signal("eaten") # Tell everyone it's been eaten
+		hide() # Hide the spinach
+		queue_free() # Remove spinach now it's been eaten!
